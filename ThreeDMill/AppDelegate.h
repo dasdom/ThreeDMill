bc3bbc4cd7a56c7052f3c645c23d80afed8aba2c
Dominik Hauser <dom@dasdom.de>
2022-08-30 19:44:31 +0200

    Initial Commit

---
+++ ThreeDMill/AppDelegate.h
@@ -0,0 +1,13 @@
+//  Created by Dominik Hauser on 30.08.22.
+//  
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreeDMill/GameViewController.h
@@ -0,0 +1,10 @@
+//  Created by Dominik Hauser on 30.08.22.
+//  
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
